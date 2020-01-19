8133a330dbdd0842dab6829f66a3aef55e522d37
Mathew Grabau <mgrabau@mathewgrabau.ca>
2020-01-19 00:25:37 -0600

    Tracking user selections for the options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
   // Property for the password and to bind it
   // Same as a constructor init.
   password = '';
 
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
   // Button clicked handler and allows us to continue using it.
   onButtonClick() {
+    console.log(`
+      About to generate a password:
+      ${this.includeLetters}
+      ${this.includeNumbers}
+      ${this.includeSymbols}
+    `);
     this.password = "AJDIOJASD";
   }
+
+
 }
